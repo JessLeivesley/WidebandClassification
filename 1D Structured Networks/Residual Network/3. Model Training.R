2aa3b224cb73c3533160fd4d2762c89ad8b3b8b9
Lewei <90325333+Le119@users.noreply.github.com>
2024-05-09 13:03:03 -0400

    Added all sets of hyperparameter in gridsearch. Ran a sunset of 20 settings. Best results updated in the model training file.

--- 1D Structured Networks/Residual Network/3. Model Training.R
+++ 1D Structured Networks/Residual Network/3. Model Training.R
@@ -17,106 +17,107 @@ for(fold in 1:1){
   
   cw<-summary(as.factor(y_train_set[,1]))[2]/summary(as.factor(y_train_set[,1]))[1]
   
-# Scaling
+  # Scaling
   scaler <- preProcess(x_train_set[,1:249], method = 'scale')
   x_train_set <- predict(scaler, x_train_set[,1:249])
   x_val_set <- predict(scaler, x_val_set[,1:249])
-
-
-
-# below need to be extracted and inputted as values so only need to change this line everytime we have new optimal values
-best_param=tibble(filters = 32, kernel_size = 7, leaky_relu = F, batch_normalization = F, batch_size = 1000)
-# best loss: 32, 7, t/f, f, 1000
-# best auc: 64, 7, f, f, 1000
-
-
-input_shape <- c(249,1)
-set_random_seed(15)
-inputs <- layer_input(shape = input_shape)
-
-block_1_output <- inputs %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu)
-
-# Adjust block_2_output to include the first convolutional layer for block_2
-block_2_prep <- block_1_output %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  add_batch_normalization(batch_normalization = best_param$batch_normalization)
-
-block_2_output <- block_2_prep %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
-  layer_add(block_1_output)
-
-# Introduce a skip from block_1_output to block_3_output
-block_3_output <- block_2_output %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
-  layer_add(block_2_output) %>%
-  layer_add(block_1_output) # Adding block_1_output as a skip to block_3_output
-
-# Continue from block_3_output to block_4_output
-block_4_output <- block_3_output %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
-  layer_add(block_3_output)
-
-# Introduce a skip from block_3_output to block_5_output
-block_5_output <- block_4_output %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
-  layer_add(block_4_output) %>%
-  layer_add(block_3_output) # Adding block_3_output as a skip to block_5_output
-
-outputs <- block_5_output %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  conv_activation_layer(filters = best_param$filters, 
-                        kernel_size = best_param$kernel_size, 
-                        leaky_relu = best_param$leaky_relu) %>%
-  add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
-  layer_max_pooling_1d(pool_size = 2) %>%
-  layer_flatten() %>%
-  layer_dense(2, activation="sigmoid")
-
-model <- keras_model(inputs, outputs)
-
-
-model %>% compile(
-  optimizer = optimizer_adam(),
-  loss = loss_categorical_crossentropy,
-  metrics = c("accuracy", tf$keras$metrics$AUC())
-)
-
-# Fit model (just resnet)
-resnet_history <- model %>% fit(
-  x_train_set, y_train_set[,c(1:2)],
-  batch_size = best_param$batch_size,
-  epochs = 100,
-  validation_data = list(x_val_set, y_val_set[,c(1:2)]),
-  class_weight = list("0"=1,"1"=cw)
-)
+  
+  
+  
+  # below need to be extracted and inputted as values so only need to change this line everytime we have new optimal values
+  best_param=tibble(filters = 16, kernel_size = 5, leaky_relu = T, batch_normalization = F, batch_size = 1200)
+  # best loss: 16, 5, t, f, 1200
+  # best epoch: 29
+  # got acc 83.59 (82.13 balanced)
+  
+  
+  input_shape <- c(249,1)
+  set_random_seed(15)
+  inputs <- layer_input(shape = input_shape)
+  
+  block_1_output <- inputs %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu)
+  
+  # Adjust block_2_output to include the first convolutional layer for block_2
+  block_2_prep <- block_1_output %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    add_batch_normalization(batch_normalization = best_param$batch_normalization)
+  
+  block_2_output <- block_2_prep %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
+    layer_add(block_1_output)
+  
+  # Introduce a skip from block_1_output to block_3_output
+  block_3_output <- block_2_output %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
+    layer_add(block_2_output) %>%
+    layer_add(block_1_output) # Adding block_1_output as a skip to block_3_output
+  
+  # Continue from block_3_output to block_4_output
+  block_4_output <- block_3_output %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
+    layer_add(block_3_output)
+  
+  # Introduce a skip from block_3_output to block_5_output
+  block_5_output <- block_4_output %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
+    layer_add(block_4_output) %>%
+    layer_add(block_3_output) # Adding block_3_output as a skip to block_5_output
+  
+  outputs <- block_5_output %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    conv_activation_layer(filters = best_param$filters, 
+                          kernel_size = best_param$kernel_size, 
+                          leaky_relu = best_param$leaky_relu) %>%
+    add_batch_normalization(batch_normalization = best_param$batch_normalization) %>%
+    layer_max_pooling_1d(pool_size = 2) %>%
+    layer_flatten() %>%
+    layer_dense(2, activation="sigmoid")
+  
+  model <- keras_model(inputs, outputs)
+  
+  
+  model %>% compile(
+    optimizer = optimizer_adam(),
+    loss = loss_categorical_crossentropy,
+    metrics = c("accuracy", tf$keras$metrics$AUC())
+  )
+  
+  # Fit model (just resnet)
+  resnet_history <- model %>% fit(
+    x_train_set, y_train_set[,c(1:2)],
+    batch_size = best_param$batch_size,
+    epochs = 100,
+    validation_data = list(x_val_set, y_val_set[,c(1:2)]),
+    class_weight = list("0"=1,"1"=cw)
+  )
 }
 
 ## edit to extract test loss, test sensitivity, test specificity, test auc
@@ -129,4 +130,3 @@ preds<-predict(model, x=x_test)
 species.predictions<-apply(preds,1,which.max)
 species.predictions<-as.factor(ifelse(species.predictions == 1, "LT", "SMB"))
 confusionMatrix(species.predictions,as.factor(test$species))
-
